a328a29745a6a618accaf151ee515006d3778d5a
sevensun8023-star <sevensun8023@gmail.com>
2026-06-18 13:00:51 +0800

    Delete mp3-player directory

--- mp3-player/app/src/main/java/com/car/mp3player/ui/MainHost.kt
+++
@@ -1,8 +0,0 @@
-package com.car.mp3player.ui
-
-interface MainHost {
-    fun playSongAt(index: Int)
-    fun switchToTab(index: Int)
-    fun scanMusic(onDone: ((Int) -> Unit)? = null)
-    fun allSongs(): List<com.car.mp3player.model.Song>
-}
