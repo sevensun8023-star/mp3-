b509e3182adafcada0f2834406bb09e0ccdbfdae
sevensun8023-star <sevensun8023@gmail.com>
2026-07-07 13:14:48 +0800

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.car.mp3player"
         minSdk = 24
         targetSdk = 34
-        versionCode = 24
-        versionName = "3.7.4"
+        versionCode = 25
+        versionName = "3.7.5"
     }
 
     buildTypes {
